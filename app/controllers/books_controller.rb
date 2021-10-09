e5723063620ef99ea62cb42892c8986afdde0a02
Vincent Walker <vincethewalker@yahoo.co.uk>
2021-10-09 16:01:16 +0100

    Added delete option

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,15 @@ class BooksController < ApplicationController
     # curl --header "Content-Type: application/json" --request POST --data '{"author": "Trevor Noah", "title": "Born a Crime"}' http://localhost:3000/books -v
   end
 
+  def destroy 
+    book = Book.find(params[:id]).destroy!
+    # We use the objects 'ID' as reference to delete (eg book id 2)
+    # curl --header "Content-Type: application/json" --request DELETE  http://localhost:3000/books/2 -v
+
+    # BELOW, there is no need to render a json object as the object has been deleted, thus render a 'head' with a 'no_content' ststus code
+    head :no_content
+  end
+
   private
 
   def book_params
