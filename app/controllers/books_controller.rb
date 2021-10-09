e8b68b8f806f91420af40f49b75593a164fcd2c5
Vincent Walker <vincethewalker@yahoo.co.uk>
2021-10-09 15:04:05 +0100

    3 Build a Post Endpoint

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,26 @@
 class BooksController < ApplicationController
+
   def index
+    #To Test this: curl http://localhost:3000/books
     render json: Book.all
+  end
+
+  def create 
+    #To Test this: curl --request POST http://localhost:3000/books
+    book = Book.new(book_params)
+
+    if book.save 
+      render json: book, status: :created # Status 201 means created
+    else
+      render json: book.errors, status: :unprocessable_entity # Status 422 means unprocessable entity
+    end
+    # Example using book_params: 
+    # curl --header "Content-Type: application/json" --request POST --data '{"author": "Trevor Noah", "title": "Born a Crime"}' http://localhost:3000/books -v
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
   end
 end
